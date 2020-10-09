cecefabfa6f8a6054f8c373b2d3ad533e84c47cf
cknotz <carlo.knotz@gmail.com>
2020-10-09 19:19:14 +0200

    update gitignore

---
+++ cube_scrape.R
@@ -0,0 +1,51 @@
+######################################################################
+# Code to scrape occupancy rate from lecube.ch & store in Google Sheet
+######################################################################
+
+# Carlo Knotz
+
+library(tidyverse)
+library(webdriver)
+library(rvest)
+
+# Invoking phantomjs() - website is dynamically created
+install_phantomjs()
+pjs_instance <- run_phantomjs()
+pjs_session <- Session$new(port = pjs_instance$port)
+
+# Scraping occupancy rate
+#########################
+url <- "http://www.lecube.ch/"
+xpath <- "//*[@id='frequentation_jauge_current_raw']"
+
+pjs_session$go(url)
+rendered_source <- pjs_session$getSource()
+cube <- read_html(rendered_source)
+
+occ <- cube %>% 
+  html_node(xpath=xpath) %>% 
+  html_text() %>% 
+  gsub("%","",.)
+
+time <- Sys.time()
+
+cube_occ <- as.data.frame(cbind(time,occ))
+
+# Export to Googlesheet
+#######################
+require(googlesheets4)
+
+sheet <- readLines("sheet.txt")
+mail <- readLines("mail.txt")
+
+gs4_auth(
+  cache = ".secrets",
+ email = mail
+)
+
+sheet_append(ss=sheet,
+             data=cube_occ,
+             sheet = "data")
+
+# Quitting R
+quit(save = "no")

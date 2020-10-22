8cafb0b73189c4946a8e97f8ff6545ed40acaad0
cknotz <carlo.knotz@gmail.com>
2020-10-22 20:27:50 +0200

    Added functions to compute daily and hourly averages for comparison

--- cube_forecast.R
+++ cube_forecast.R
@@ -20,15 +20,29 @@ gs4_auth(email = mail,
 # Download data
 cubedata <- read_sheet(ss=sheet)
 
+# Remove first observation - originally test
+cubedata <- cubedata[2:nrow(cubedata),]
+
 # Change datetime format
 cubedata$time <- substr(cubedata$time, 1, 16)
   cubedata$time <- as.POSIXct(cubedata$time)
   cubedata$day <- weekdays(cubedata$time,
                            abbreviate = T)
+  cubedata$hour <- format(cubedata$time,
+                          format = "%H:%M:%S")
   
+# Setting values to zero during closed hours
+cubedata$occ[cubedata$hour %in% c("23:00:00","23:30:00","00:00:00","00:30:00","01:00:00",
+                                  "01:30:00","02:00:00","02:30:00","03:00:00","03:30:00",
+                                  "04:00:00","04:30:00","05:00:00","05:30:00","06:00:00",
+                                  "06:30:00","07:00:00","07:30:00","08:00:00","08:30:00",
+                                  "09:00:00","09:30:00") ] <- 0
+
+cubedata$occ[cubedata$hour %in% c("20:30:00","21:00:00","21:30:00","22:00:00","22:30:00")
+             & cubedata$day %in% c("Sat","Sun")] <- 0
+    
 # Linearly interpolating missing values
 cubedata$occ_inter <- zoo::na.approx(cubedata$occ)
-
 
 # Graph
 ggplot(data=cubedata,aes(x=time)) +
@@ -39,5 +53,18 @@ ggplot(data=cubedata,aes(x=time)) +
   ylab("Occupancy (%)") +
   xlab("") +
   theme_bw()
+
+
+# Averages per day
+##################
+
+aggregate(cubedata$occ_inter,list(cubedata$hour),mean) # average per hour
+
+aggregate(cubedata$occ_inter,list(cubedata$day),mean) # average per day
+
+aggregate(cubedata$occ_inter,list(cubedata$day,cubedata$hour),mean) # average per day & hour
+
+res <- aggregate(cubedata$occ_inter,list(cubedata$day,cubedata$hour),mean) # Specific day/time average
+  res$x[res$Group.1 == "Fri" & res$Group.2 == "14:00:00"]
 
 

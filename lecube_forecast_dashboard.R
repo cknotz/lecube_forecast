c08db73b3a0fe1a2dfb81bac623a129e43995b5a
cknotz <carlo.knotz@gmail.com>
2020-10-29 19:55:54 +0100

    Added background info

--- lecube_forecast_dashboard.R
+++ lecube_forecast_dashboard.R
@@ -28,49 +28,49 @@ options(gargle_oauth_cache = ".secrets")
 gs4_auth(email = mail,
          cache = ".secrets")
 
-#Download data
-cubedata <- read_sheet(ss=sheet)
-
-# Remove first observation - originally test
-cubedata <- cubedata[2:nrow(cubedata),]
-
-# Change datetime format
-cubedata$time <- substr(cubedata$time, 1, 16)
-  cubedata$time <- as.POSIXct(cubedata$time, tz="Europe/Paris")
-  cubedata$day <- weekdays(cubedata$time,
-                           abbreviate = T)
-  cubedata$hour <- format(cubedata$time,
-                          format = "%H:%M:%S")
-
-# Setting values to zero during closed hours
-cubedata$occ[cubedata$hour %in% c("23:00:00","23:30:00","00:00:00","00:30:00","01:00:00",
-                                  "01:30:00","02:00:00","02:30:00","03:00:00","03:30:00",
-                                  "04:00:00","04:30:00","05:00:00","05:30:00","06:00:00",
-                                  "06:30:00","07:00:00","07:30:00","08:00:00","08:30:00",
-                                  "09:00:00","09:30:00") ] <- 0
-
-cubedata$occ[cubedata$hour %in% c("20:30:00","21:00:00","21:30:00","22:00:00","22:30:00")
-             & cubedata$day %in% c("Sat","Sun")] <- 0
-
-# Linearly interpolating missing values
-cubedata$occ_inter <- zoo::na.approx(cubedata$occ)
-
-# Comparison figures
-compa <- aggregate(cubedata$occ_inter,list(cubedata$day,cubedata$hour),mean) # Specific day/time averages
-norm <- compa$x[compa$Group.1 == tail(cubedata$day,1) & compa$Group.2 == tail(cubedata$hour,1)]
-
-compa <- aggregate(cubedata$occ_inter,list(cubedata$day,cubedata$hour),max) # Specific day/time averages
-max <- compa$x[compa$Group.1 == tail(cubedata$day,1) & compa$Group.2 == tail(cubedata$hour,1)]
-
+# #Download data
+# cubedata <- read_sheet(ss=sheet)
+# 
+# # Remove first observation - originally test
+# cubedata <- cubedata[2:nrow(cubedata),]
+# 
+# # Change datetime format
+# cubedata$time <- substr(cubedata$time, 1, 16)
+#   cubedata$time <- as.POSIXct(cubedata$time, tz="Europe/Paris")
+#   cubedata$day <- weekdays(cubedata$time,
+#                            abbreviate = T)
+#   cubedata$hour <- format(cubedata$time,
+#                           format = "%H:%M:%S")
+# 
+# # Setting values to zero during closed hours
+# cubedata$occ[cubedata$hour %in% c("23:00:00","23:30:00","00:00:00","00:30:00","01:00:00",
+#                                   "01:30:00","02:00:00","02:30:00","03:00:00","03:30:00",
+#                                   "04:00:00","04:30:00","05:00:00","05:30:00","06:00:00",
+#                                   "06:30:00","07:00:00","07:30:00","08:00:00","08:30:00",
+#                                   "09:00:00","09:30:00") ] <- 0
+# 
+# cubedata$occ[cubedata$hour %in% c("20:30:00","21:00:00","21:30:00","22:00:00","22:30:00")
+#              & cubedata$day %in% c("Sat","Sun")] <- 0
+# 
+# # Linearly interpolating missing values
+# cubedata$occ_inter <- zoo::na.approx(cubedata$occ)
+# 
+# # Comparison figures
+# compa <- aggregate(cubedata$occ_inter,list(cubedata$day,cubedata$hour),mean) # Specific day/time averages
+# norm <- compa$x[compa$Group.1 == tail(cubedata$day,1) & compa$Group.2 == tail(cubedata$hour,1)]
+# 
+# compa <- aggregate(cubedata$occ_inter,list(cubedata$day,cubedata$hour),max) # Specific day/time averages
+# max <- compa$x[compa$Group.1 == tail(cubedata$day,1) & compa$Group.2 == tail(cubedata$hour,1)]
+# 
 
 # Setting up dashboard
 ######################
 
 ui <- dashboardPage(
   dashboardHeader(title = "Le Cube Forecast"),
-  dashboardSidebar(collapsed = T,
+  dashboardSidebar(collapsed = F,
     sidebarMenu(menuItem("Current occupancy & forecast",tabName = "data", icon = icon("dashboard")),
-                menuItem("Background info", tabName = "info", icon = icon("info-circle", lib = "font-awesome")))),
+                menuItem("Background info", tabName = "info", icon = icon("info-circle", lib = "font-awesome"), selected = T))),
   dashboardBody(
     tags$style(type="text/css",
     ".js-irs-0 .irs-single, .js-irs-0 .irs-bar-edge, .js-irs-0 .irs-bar {background: #f08c00;border-color: #f08c00;}
@@ -119,7 +119,38 @@ ui <- dashboardPage(
                     )
               )
               ),
-      tabItem(tabName = "info")))
+      tabItem(tabName = "info",
+              fluidRow(
+                box(width = 12, solidHeader = T,collapsible = F,title = "Background & Methods",
+                    HTML("<p>Crowded bouldering gyms aren't fun - and especially now that there is a significant risk that someone
+                         in the gym might carry and spread the new coronavirus. Luckily, my local gym (Le Cube in Lausanne) has
+                         a neat little dial on their website (<a href='http://www.lecube.ch/' target='_blank'>lecube.ch</a>) that tells how full the gym is at any given point in time.</p>
+                         
+                         <p>But why stop there when we also have tools to forecast how busy the gym will be over the next hours?
+                         This is the idea behind this dashboard.</p>
+                         
+                         <p>The data for this dashboard are scraped from the lecube.ch website every full and half hour and stored
+                         in the cloud (this started on the afternoon of October 10, 2020 and is ongoing).</p>
+                         
+                         <p>Now that several weeks worth of data have been collected, I built this dashboard, which retrieves the data, shows simple descriptive
+                         information and features a forecast function to estimate how busy the gym will be over the next five to 36 hours.</p>
+                         
+                         <p>The forecast is estimated anew every time the dashboard is started and the '(Re-)run forecast' button is klicked.
+                         The algorithm behind the forecast is a neural network for time-series data, specifically the <code>nnetar()</code>
+                         function contained in the <code>forecast</code>-package (see <a target='_blank' href='https://otexts.com/fpp2/'>Hyndman & Athanasopoulos, 2018</a>), which estimates a feed-forward neural network with a single hidden node and a variable number of lagged values 
+                         as inputs. The optimal configuration is determined automatically on a case-by-case basis.</p>
+                         
+                         <p>The code to scrape the data, sample code for the forecasting function, and the code for this dashboard
+                         are deposited on my <a href='https://github.com/cknotz/lecube_forecast' target='_blank'>GitHub profile</a>.</p>
+                         
+                         <p>The next step will be to determine an optimal forecasting model by comparing the performances of a number of 
+                         different estimators against each other. This will be done as soon as more data have been collected and there is enough
+                         information for the estimators to learn the more subtle seasonal features in the data, for instance the difference between
+                         weekends and weekdays.</p>
+                         
+                         <p>I am not affiliated with Le Cube (apart from my membership, which I paid for myself) and I hope they will
+                         forgive me for using up a bit of their bandwith when I scrape their data.</p>"))
+              ))))
 )
 
 server <- function(input, output, session) { 

0cf0009fac0ed031fa6c55f25d03544f8096fdcb
cknotz <carlo.knotz@gmail.com>
2020-10-30 19:53:05 +0100

    Fixed bug (part of code commented out)

--- lecube_forecast_dashboard.R
+++ lecube_forecast_dashboard.R
@@ -28,40 +28,40 @@ options(gargle_oauth_cache = ".secrets")
 gs4_auth(email = mail,
          cache = ".secrets")
 
-# #Download data
-# cubedata <- read_sheet(ss=sheet)
-# 
-# # Remove first observation - originally test
-# cubedata <- cubedata[2:nrow(cubedata),]
-# 
-# # Change datetime format
-# cubedata$time <- substr(cubedata$time, 1, 16)
-#   cubedata$time <- as.POSIXct(cubedata$time, tz="Europe/Paris")
-#   cubedata$day <- weekdays(cubedata$time,
-#                            abbreviate = T)
-#   cubedata$hour <- format(cubedata$time,
-#                           format = "%H:%M:%S")
-# 
-# # Setting values to zero during closed hours
-# cubedata$occ[cubedata$hour %in% c("23:00:00","23:30:00","00:00:00","00:30:00","01:00:00",
-#                                   "01:30:00","02:00:00","02:30:00","03:00:00","03:30:00",
-#                                   "04:00:00","04:30:00","05:00:00","05:30:00","06:00:00",
-#                                   "06:30:00","07:00:00","07:30:00","08:00:00","08:30:00",
-#                                   "09:00:00","09:30:00") ] <- 0
-# 
-# cubedata$occ[cubedata$hour %in% c("20:30:00","21:00:00","21:30:00","22:00:00","22:30:00")
-#              & cubedata$day %in% c("Sat","Sun")] <- 0
-# 
-# # Linearly interpolating missing values
-# cubedata$occ_inter <- zoo::na.approx(cubedata$occ)
-# 
-# # Comparison figures
-# compa <- aggregate(cubedata$occ_inter,list(cubedata$day,cubedata$hour),mean) # Specific day/time averages
-# norm <- compa$x[compa$Group.1 == tail(cubedata$day,1) & compa$Group.2 == tail(cubedata$hour,1)]
-# 
-# compa <- aggregate(cubedata$occ_inter,list(cubedata$day,cubedata$hour),max) # Specific day/time averages
-# max <- compa$x[compa$Group.1 == tail(cubedata$day,1) & compa$Group.2 == tail(cubedata$hour,1)]
-# 
+#Download data
+cubedata <- read_sheet(ss=sheet)
+
+# Remove first observation - originally test
+cubedata <- cubedata[2:nrow(cubedata),]
+
+# Change datetime format
+cubedata$time <- substr(cubedata$time, 1, 16)
+  cubedata$time <- as.POSIXct(cubedata$time, tz="Europe/Paris")
+  cubedata$day <- weekdays(cubedata$time,
+                           abbreviate = T)
+  cubedata$hour <- format(cubedata$time,
+                          format = "%H:%M:%S")
+
+# Setting values to zero during closed hours
+cubedata$occ[cubedata$hour %in% c("23:00:00","23:30:00","00:00:00","00:30:00","01:00:00",
+                                  "01:30:00","02:00:00","02:30:00","03:00:00","03:30:00",
+                                  "04:00:00","04:30:00","05:00:00","05:30:00","06:00:00",
+                                  "06:30:00","07:00:00","07:30:00","08:00:00","08:30:00",
+                                  "09:00:00","09:30:00") ] <- 0
+
+cubedata$occ[cubedata$hour %in% c("20:30:00","21:00:00","21:30:00","22:00:00","22:30:00")
+             & cubedata$day %in% c("Sat","Sun")] <- 0
+
+# Linearly interpolating missing values
+cubedata$occ_inter <- zoo::na.approx(cubedata$occ)
+
+# Comparison figures
+compa <- aggregate(cubedata$occ_inter,list(cubedata$day,cubedata$hour),mean) # Specific day/time averages
+norm <- compa$x[compa$Group.1 == tail(cubedata$day,1) & compa$Group.2 == tail(cubedata$hour,1)]
+
+compa <- aggregate(cubedata$occ_inter,list(cubedata$day,cubedata$hour),max) # Specific day/time averages
+max <- compa$x[compa$Group.1 == tail(cubedata$day,1) & compa$Group.2 == tail(cubedata$hour,1)]
+
 
 # Setting up dashboard
 ######################
@@ -70,7 +70,7 @@ ui <- dashboardPage(
   dashboardHeader(title = "Le Cube Forecast"),
   dashboardSidebar(collapsed = F,
     sidebarMenu(menuItem("Current occupancy & forecast",tabName = "data", icon = icon("dashboard")),
-                menuItem("Background info", tabName = "info", icon = icon("info-circle", lib = "font-awesome"), selected = T))),
+                menuItem("Background info", tabName = "info", icon = icon("info-circle", lib = "font-awesome")))),
   dashboardBody(
     tags$style(type="text/css",
     ".js-irs-0 .irs-single, .js-irs-0 .irs-bar-edge, .js-irs-0 .irs-bar {background: #f08c00;border-color: #f08c00;}
